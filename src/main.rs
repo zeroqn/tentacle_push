7e1c94be2cd7ac1da8b801cb7c0cc72a1414c7d1
zero.qn <zero.qn@outlook.com>
2020-04-26 21:45:16 +0800

    format

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,13 @@ use tentacle::{builder::ServiceBuilder, secio::SecioKeyPair, service::TargetProt
 async fn main() {
     env_logger::init();
 
-    let push_size = std::env::args().nth(2).map(|s| s.parse::<usize>().ok()).flatten();
+    let push_size = std::env::args()
+        .nth(2)
+        .map(|s| s.parse::<usize>().ok())
+        .flatten();
     let push_bytes = push_bytes::PushBytes::new(1.into(), push_size);
-    let mut service = ServiceBuilder::default().insert_protocol(push_bytes)
+    let mut service = ServiceBuilder::default()
+        .insert_protocol(push_bytes)
         .key_pair(SecioKeyPair::secp256k1_generated())
         .build(());
 
@@ -18,7 +22,10 @@ async fn main() {
             panic!("server push size is none");
         }
 
-        service.listen("/ip4/0.0.0.0/tcp/2233".parse().expect("listen address")).await.expect("listen");
+        service
+            .listen("/ip4/0.0.0.0/tcp/2233".parse().expect("listen address"))
+            .await
+            .expect("listen");
 
         loop {
             if service.next().await.is_none() {
@@ -28,10 +35,15 @@ async fn main() {
     } else {
         let server = {
             let server = std::env::args().nth(1).expect("missing server ip");
-            format!("/ip4/{}/tcp/2233", server).parse().expect("server address")
+            format!("/ip4/{}/tcp/2233", server)
+                .parse()
+                .expect("server address")
         };
 
-        service.dial(server, TargetProtocol::All).await.expect("dial");
+        service
+            .dial(server, TargetProtocol::All)
+            .await
+            .expect("dial");
 
         loop {
             if service.next().await.is_none() {
